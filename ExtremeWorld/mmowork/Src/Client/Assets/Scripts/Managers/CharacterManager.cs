c1d453d84188509905dd1d2782c95e90b932c1ab
GochenRyan <rangaocheng@outlook.com>
2021-06-28 21:14:47 +0800

    小地图、角色离开逻辑

--- ExtremeWorld/mmowork/Src/Client/Assets/Scripts/Managers/CharacterManager.cs
+++ ExtremeWorld/mmowork/Src/Client/Assets/Scripts/Managers/CharacterManager.cs
@@ -39,7 +39,12 @@ namespace Managers
 
         public void Clear()
         {
-
+            int[] keys = this.Characters.Keys.ToArray();
+            foreach(var key in keys)
+            {
+                this.RemoveCharacter(key);
+            }
+            this.Characters.Clear();
         }
 
         public void AddCharacter(SkillBridge.Message.NCharacterInfo cha)
@@ -47,6 +52,7 @@ namespace Managers
             Debug.LogFormat("AddCharacter: {0}: {1} Map: {2} Entity: {3}", cha.Id, cha.Name, cha.mapId, cha.Entity.String());
             Character character = new Character(cha);
             this.Characters[cha.Id] = character;
+            EntityManager.Instance.AddEntity(character);
 
             if (OnCharacterEnter != null)
             {
